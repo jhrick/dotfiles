91ded17811f2180308c3c08fa337e6a453ecb2f3
Jorge Henrique <hj.rickcp@gmail.com>
2024-01-19 13:59:56 -0300

    mininal and clean terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-   starship init fish | source
-   set --export PATH $HOME/.cargo/bin $PATH
-
+    starship init fish | source
 end
-
-function pendrive
-  if test -z "$argv"
-    cd /media/rick/Ventoy
-  else
-    cd /media/rick/Ventoy/$argv
-  end
-end
- 
